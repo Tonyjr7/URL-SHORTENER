46f23619c3127a65008a5d75ad0f15788d8ec16b
Tonyjr7 <anthonyayokunle777@gmail.com>
2025-10-19 01:41:19 +0100

    feat: added cors

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.urls import client as db
 from utils.response import Response
 from routes.shorten import router as shorten_router
 from routes.fetch import router as fetch_router
 
-app = FastAPI()
+app = FastAPI(title="URL Shortener Service", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
